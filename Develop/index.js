d9f26f99efe2949cef1b039c5343b8d767af0816
Nico P <nicopetkas@gmail.com>
2024-02-23 11:30:50 -0800

    js update

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter project description",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Select your license:",
+        choices: ["Apache", "GNU", "MIT"],
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message: "What is your Github username",
+    },
+    {
+        type: 'input',
+        name: 'GithubUrl',
+        message: "What is your Github URL",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readmeContent = generateMarkdown(fileName, data);
+
+    fs.writeFile('README.md', readmeContent, (err) =>
+    err ? console.log(err) : console.log("Success"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+    writeToFile;
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,94 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'APACHE':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'APACHE':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+
+  if (!licenseBadge || !licenseLink) {
+    return ''; // Return an empty string if the license is not recognized
+  }
+
+  return `## License
+
+This project is licensed under the [${license} License](${licenseLink}) - ${licenseBadge}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+## Description
+  ${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+
+## Installation
+
+1. Clone the repository \`git clone git@github.com:${data.github}/${data.githubUrl}.git\`
+2. Navigate to the project directory \`cd ./${data.githubUrl}\`
+3. Install dependencies \`npm i inquirer@v8.2.4\`
+4. Run the application \`node index.js\`
+
+## Usage
+${usage}
+
+## License
+This project is licensed under the ${renderLicenseSection(license)}
+
+## Contributing
+
+To contribute:
+
+1. Fork the repository
+2. Create a new branch
+3. Make your changes
+4. Submit a pull request
+
+
+## Questions
+
+Please contact for any questions:
+
+Github Profile: [${data.github}](https://github.com/${data.github})
+
+Link to Repo: [${data.githubRepo}](https://github.com/${data.github}/${data.githubRepo})
+
+Email: [${data.email}](emailto:${data.email})
 `;
 }
 
